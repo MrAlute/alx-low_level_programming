54ec36ed3ad4d96efed8704821e388f5449adc27
MrAlute <abichioduye@gmail.com>
2023-02-17 17:41:41 -0800

    updated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Description: Print numbers from 0 to 99.
- * Numbers must be separated by commas followed by a space.
- * You can only use `putchar` to print to console.
- * You can only use `putchar` up to 5 times.
- * You are not allowed to use variables of type `char`.
- * Return: 0
+ * main - a program that prints all possible different combination of 2 digits
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int i, j, k;
+        int n, m;
 
-	for (i = 0; i < 100; i++)
-	{
-		j = i / 10;
-		k = i % 10;
-
-		putchar(j + '0');
-		putchar(k + '0');
-
-		if (i < 99)
-		{
-			putchar(44);
-			putchar(32);
-		}
-	}
-	putchar('\n');
-
-	return (0);
+        for (n = 48; n <= 56; n++)
+        {
+                for (m = 49; m <= 57; m++)
+                {
+                        if (m > n)
+                        {
+                                putchar(n);
+                                putchar(m);
+                                if (n != 56 || m != 57)
+                                {
+                                        putchar(',');
+                                        putchar(' ');
+                                }
+                        }
+                }
+        }
+        putchar('\n');
+        return (0);
 }
