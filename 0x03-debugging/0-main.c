e6f84dd8f373bf7df64b97679ab84e5408908e85
MrAlute <abichioduye@gmail.com>
2023-02-23 08:55:25 -0800

    added

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,23 +5,19 @@
  * positive_or_negative - Getting a number and checking if
  * the number is positive or negative.
  * @i:int type letter
- * Return: 0
  */
 
 /* main - function */
 void positive_or_negative(int i)
 {
-	int n;
+	i = 0;
 
-	n = 0;
-
-	if (n < 0)
+	if (i < 0)
 	{
-		printf("%d is negative\n", n);
-	} else if (n == 0)
+		printf("%d is negative\n", i);
+	} else if (i == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", i);
 	} else
-		printf("%d is positive\n", n);
-	return (0);
+		printf("%d is positive\n", i);
 }
