a3f8643f5da635817b8785ce30f497542e7ad16c
MrAlute <abichioduye@gmail.com>
2023-02-23 06:14:41 -0800

    updated

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print number 0 to 9
+ *@i: int type
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+		_putchar("\n);
+		return (0);
+	}
+}
+
