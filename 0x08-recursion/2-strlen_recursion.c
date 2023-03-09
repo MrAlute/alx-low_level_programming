a09de8709f6b6c1f80afcad37372ea912cccc31e
MrAlute <abichioduye@gmail.com>
2023-03-08 16:43:01 -0800

    updated

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,13 +9,11 @@
 
 int _strlen_recursion(char *s)
 {
-	if (s == "\0")
+	if (*s == '\0')
 	{
-		return;
+		return (0);
 	}
 
 	s++;
-	_strlen_recursion(s) + 1;
-
-	return (s);
+	return (_strlen_recursion(s) + 1);
 }
