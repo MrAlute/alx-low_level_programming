47df24b52633a480a07a7de2f0e47eeb344f2f81
MrAlute <abichioduye@gmail.com>
2023-03-10 17:16:16 -0800

    updated

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -5,6 +5,41 @@
 #include <stdbool.h>
 
 /**
+ * coinConverter - Helper function that does all the mathematics
+ * @i: Passed in variable from main for calculations
+ * Return: The number of coins needed minimum for the passed in variable
+ */
+int coinConverter(int i)
+{
+	int count = 0;
+
+	while (i != 0)
+	{
+		if (i % 10 == 9 || i % 10 == 7)
+			i -= 2;
+		else if (i % 25 == 0)
+			i -= 25;
+		else if (i % 10 == 0)
+			i -= 10;
+		else if (i % 5 == 0)
+			i -= 5;
+		else if (i % 2 == 0)
+		{
+			if (i % 10 == 6)
+				i -= 1;
+			else
+				i -= 2;
+		}
+		else
+			i -= 1;
+
+		count++;
+	}
+
+	return (count);
+}
+
+/**
  * main - Takes in exactly one argument for minimum coin count
  * @argc: Number of command line arguments
  * @argv: Array name
@@ -12,9 +47,9 @@
  */
 int main(int argc, char *argv[])
 {
-	int num, j, result;
+	int i, coin;
 
-	int coins[] = {25, 10, 5, 2, 1};
+	coin = 0;
 
 	if (argc != 2)
 	{
@@ -22,22 +57,16 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	num = atoi(argv[1]);
+	i = atoi(argv[1]);
 
-	if (num < 0)
-	{
+	if (i < 0)
 		printf("0\n");
-		return (0);
-	}
-n
-	for (j = 0; j < 5 && num >= 0; j++)
+	else
 	{
-		while (num >= coins[j])
-		{
-			result++;
-			num -= coins[j];
-		}
+		coin = coinConverter(i);
+
+		printf("%d\n", coin);
 	}
-	printf("%d\n", result);
+
 	return (0);
 }
